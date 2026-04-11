e161c3ff6a7d1f577e1103bf9f78c04b19810e6c
A1D2I3T4 <113341778+A1D2I3T4@users.noreply.github.com>
2026-04-11 09:51:23 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,8 +32,8 @@ function createBot() {
   // === GREETING SYSTEM ===
   const greetings = [
     " Sub to DeathVoxel {player} on Youtube"
-    " Get kits from jonarchyshop.weebly.com, {player}!"
-    " Found your stash {player}, just kidding!"
+    " Get kits from jonarchyshop.weebly.com: {player}!"
+    " Found your stash {player}... just kidding!"
     " A player joined! Who is it? It's {player}⚡⚡"
     " Meet {player} ⚡ !!! Be friendly!"
     " Make space for ⚡ {player}!"
